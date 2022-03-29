7b45247967cc7de34a5e28cccd15108500585b4f
Abhishek <ac1231998@gmail.com>
2022-03-29 08:40:08 +0530

    DeadlineReceiver is added -now user will get deadline notification/alerts when deadline is near

--- app/src/main/java/com/example/taskmanager/utils/Utlis.kt
+++ app/src/main/java/com/example/taskmanager/utils/Utlis.kt
@@ -1,7 +1,10 @@
 package com.example.taskmanager.utils
 
+import android.app.PendingIntent
+import android.os.Build
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 fun getFormattedTime(timeInMillis:Long) : String{
     val format = SimpleDateFormat("dd MMM yyyy hh:mm a", Locale.getDefault())
@@ -19,4 +22,20 @@ enum class Theme {
     SYSTEM,
     DARK,
     LIGHT
+}
+
+
+fun getTimeForAlarm(deadline: Long): Long {
+    val currentTime = System.currentTimeMillis()
+    val oneDay = TimeUnit.DAYS.toMillis(1)
+    val oneHour = TimeUnit.HOURS.toMillis(1)
+
+    return if (deadline - oneDay > currentTime) deadline - oneDay
+    else deadline - oneHour
+}
+
+fun getPendingIntentFlag() : Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+    else PendingIntent.FLAG_UPDATE_CURRENT
 }
