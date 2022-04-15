e570893bfb6ebb692b6ebb9fc9f795af5ebec8f2
Abhishek <ac1231998@gmail.com>
2022-04-15 21:34:47 +0530

    Login and Signup

---
+++ app/src/main/java/com/example/taskmanager/ui/home/auth/Login.kt
@@ -0,0 +1,14 @@
+package com.example.taskmanager.ui.home.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.taskmanager.R
+
+
+class Login : Fragment(R.layout.fragment_login) {
+
+
+}
